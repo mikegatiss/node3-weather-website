72c54923e36374cd5a1746de6b9a3de7c71e6b4a
P611209 <mike.gatiss@vitality.co.uk>
2021-05-11 16:42:18 +0100

    Added high and low temp to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/8abb59a1e17641d52eda0664fdaab1a5/' + latitude + ',' + longitude
     console.log(url)
-    request({ url, json: true }, (error, { body } = {}) => {
+    request({ url, json: true, strictSSL:false }, (error, { body } = {}) => {
         if (error) {
             console.log(error)
             callback('Unable to connect to weather service!', undefined)
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             console.log(body.error)
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out with a high today of ' + body.daily.data[0].temperatureHigh + ' and a low tonight of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
